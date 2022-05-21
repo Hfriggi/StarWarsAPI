97df7db1bb1c3eb6d0bc5a93594ef8af7d4746b7
Hfriggi <henriquefriggi1@gmail.com>
2022-05-21 02:04:35 -0300

    pilots

--- app.js
+++ app.js
@@ -1,14 +1,28 @@
-let button = document.querySelector('#button1')
-let name = document.querySelector('#planet')
-
 document.getElementById('button1').onclick = function(){
     var planetId = document.getElementById('planetData').value;
+    var planetId2 = document.getElementById('planetData2').value;
     axios.get('https://swapi.dev/api/planets').then(function(response){
-        updateInfo(response.data.results)
+        updateInfoPlanets(response.data.results)
         console.log(planetId)
-    })
-    
-    function updateInfo(data) {
+        console.log(planetId2)
+    }
+    )
+    var pilotId = document.getElementById('pilotData').value;
+    var copilotId = document.getElementById('copilotData').value;
+    axios.get('https://swapi.dev/api/people').then(function(response){
+        updateInfoPilots(response.data.results)
+        console.log(pilot)
+        console.log(copilot)
+    }
+    )
+
+    function updateInfoPlanets(data) {
         planet.innerText = data[planetId].name
+        planet2.innerText = data[planetId2].name
+    }
+
+    function updateInfoPilots(data) {
+        pilot.innerText = data[pilotId].name
+        copilot.innerText = data[copilotId].name
     }
 }
