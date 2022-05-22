f021f15ad2092774b13dd6632e38b6205d5b2ca9
Hfriggi <henriquefriggi1@gmail.com>
2022-05-21 22:40:15 -0300

    ship

--- app.js
+++ app.js
@@ -1,37 +1,46 @@
 document.getElementById('button1').onclick = function(){
     var planetId = document.getElementById('planetData').value;
     var planetId2 = document.getElementById('planetData2').value;
-    axios.get('https://swapi.dev/api/planets').then(function(response){
-        updateInfoPlanets(response.data.results)
-        console.log(planetId)
-        console.log(planetId2)
-    }
-    )
+    var urlPlanet1 = 'https://swapi.dev/api/planets/' + planetId;
+    var urlPlanet2 = 'https://swapi.dev/api/planets/' + planetId2;
+    $.getJSON(urlPlanet1, function(planetData)
+    {
+        $.getJSON(urlPlanet2, function(planetData2)
+        {
+            console.log(planetData.name)
+            console.log(planetData2.name)
+            planet.innerText = planetData.name
+            planet2.innerText = planetData2.name
+        })
+    });
+
     var pilotId = document.getElementById('pilotData').value;
     var copilotId = document.getElementById('copilotData').value;
-    axios.get('https://swapi.dev/api/people').then(function(response){
-        updateInfoPilots(response.data.results)
-        console.log(pilot)
-        console.log(copilot)
-    }
-    )
+    var url = 'https://swapi.dev/api/people/' + pilotId;
+    var url2 = 'https://swapi.dev/api/people/' + copilotId;
+    $.getJSON(url, function(data)
+        {
+            $.getJSON(url2, function(data2)
+            {
+                console.log(data.name)
+                console.log ('pilot species is:', data.species)
+                console.log(data2.name)
+                console.log ('copilot species is:', data2.species)
+                pilot.innerText = data.name
+                copilot.innerText = data2.name
 
-    function updateInfoPlanets(data) {
-        planet.innerText = data[planetId].name
-        planet2.innerText = data[planetId2].name
-    }
+                if (data.species == 'https://swapi.dev/api/species/2/')
+                {
+                    console.log('one of your pilots id a droid. Ready to go!')
+                }
+            })
+        });
 
-    function updateInfoPilots(data) {
-        pilot.innerText = data[pilotId].name
-        copilot.innerText = data[copilotId].name
-    }
-
-    function teste () {
-        var pilotId = document.getElementById('pilotData').value;
-        var copilotId = document.getElementById('copilotData').value;
-
-        ija = axios.get('https://swapi.dev/api/people/' + pilotId + '.name')
-        console.log('test: '+ija.name)
-    }
-    teste ();
+    var shipId = document.getElementById('shipData').value;
+    var urlShip = 'https://swapi.dev/api/starships/' + shipId;
+    $.getJSON(urlShip, function(dataShip)
+        {
+            console.log(shipId)
+            shipPrint.innerText = dataShip.name
+        })
 }
